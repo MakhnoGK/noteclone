49f51af2e38da7f1b921c79236e510982f112695
Hryhorii Khyzhniakov <soults15@gmail.com>
2021-02-05 15:38:58 +0200

    Change note saving behaviour
    Edited note now saved after 500 ms after user stops typing

--- client/src/features/notes/NoteEditor.tsx
+++ client/src/features/notes/NoteEditor.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import ReactQuill from 'react-quill';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectCurrentNote } from './noteSlice';
@@ -6,38 +6,50 @@ import { useNoteEditor } from '../../hooks/useNoteEditor';
 import placeholderIcon from '../../assets/images/blank-document.svg';
 import 'react-quill/dist/quill.snow.css';
 import { saveNoteAsync } from './asyncFunctions';
+import { debounce } from 'lodash';
 
 const NoteEditor = () => {
     const currentNote = useSelector(selectCurrentNote);
-
     const dispatch = useDispatch();
+
     const [editorState, setEditorState, title, setTitle] = useNoteEditor();
 
-    const saveNote = () => {
-        dispatch(saveNoteAsync({ title, text: editorState }));
+    const saveNote = (title: string, text: string) => {
+        dispatch(saveNoteAsync({ title, text }));
     };
 
+    const saveCallback = useRef(debounce((title, text) => {
+        saveNote(title, text);
+    }, 1000)).current;
+
     // Hide editor if user doesnt have notes
-    if (!currentNote) return <div className="editor-placeholder">
-        <img src={placeholderIcon} alt="Editor placeholder" />
-    </div>
+    if (!currentNote)
+        return (
+            <div className="editor-placeholder">
+                <img src={placeholderIcon} alt="Editor placeholder" />
+            </div>
+        );
 
     return (
         <>
             <input
                 type="text"
                 value={title}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
-                onBlur={saveNote}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                   {
+                       setTitle(e.target.value)
+                       saveCallback(e.target.value, editorState);
+                   } 
+                }
                 className="editor-content__title"
             />
 
             <ReactQuill
                 value={editorState}
-                onChange={(value) => setEditorState(value)}
-                onBlur={() =>
-                    dispatch(saveNoteAsync({ title, text: editorState }))
-                }
+                onChange={(value) => {
+                    setEditorState(value)
+                    saveCallback(title, value);
+                }}
                 style={{ flex: 1 }}
             />
         </>
